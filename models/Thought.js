e378023ac9e184315c5b5c1d20d831b632806160
GordonOrr <103220757+GordonOrr@users.noreply.github.com>
2022-09-22 14:48:30 -0400

    Create model Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
